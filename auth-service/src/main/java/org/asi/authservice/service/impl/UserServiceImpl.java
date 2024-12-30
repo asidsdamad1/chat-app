b0b4dd9625162cb8f1c6031a5ec2deb714890dec
hieunt <trunghieu.ngo612@gmail.com>
2024-12-30 23:56:14 +0700

    feat: create user

--- auth-service/src/main/java/org/asi/authservice/service/impl/UserServiceImpl.java
+++ auth-service/src/main/java/org/asi/authservice/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package org.asi.authservice.service.impl;
 
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.asi.authservice.model.User;
 import org.asi.authservice.repository.UserRepository;
 import org.asi.authservice.service.UserService;
+import org.asi.exceptionutils.AlreadyExistsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public void createUser(String username, String password, String email, String firstName, String lastName) {
         if (userRepository.existsByUsernameIgnoreCase(username)) {
-            throw Alread
+            throw new AlreadyExistsException("User with username %s already exists".formatted(username));
+        }
+
+        if (userRepository.existsByUsernameIgnoreCase(email)) {
+            throw new AlreadyExistsException("User with username %s already exists".formatted(email));
         }
 
         var user = User.builder()
