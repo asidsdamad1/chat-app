5123ab68d0064e088e295cd57a65c7d16520323c
hieunt <trunghieu.ngo612@gmail.com>
2025-04-01 23:23:32 +0700

    register chat-service to eureka

---
+++ chat-service/src/main/java/org/asi/chatservice/exception/handler/CustomExceptionsHandler.java
@@ -0,0 +1,8 @@
+package org.asi.chatservice.exception.handler;
+
+import org.asi.exceptionutils.CommonExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionsHandler implements CommonExceptionHandler {
+}
--- chat-service/src/main/java/org/asi/chatservice/handler/CustomExceptionsHandler.java
+++
@@ -1,8 +0,0 @@
-package org.asi.chatservice.handler;
-
-import org.asi.exceptionutils.CommonExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class CustomExceptionsHandler implements CommonExceptionHandler {
-}
